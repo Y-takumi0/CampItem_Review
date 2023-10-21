25c5b9c52953e7b05ada50c744b90cdba51082e9
Y-takumi0 <134852542+Y-takumi0@users.noreply.github.com>
2023-10-21 02:04:07 +0000

    routing作成

--- Item_Review/config/routes.rb
+++ Item_Review/config/routes.rb
@@ -1,5 +1,29 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_for :admins
+devise_for :users, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+ scope module: :public do
+    resources :items, only: [:index, :show, :new, :edit, :create, :update]
+    resources :users, only: [:show, :edit, :update]
+    resources :reviews, only: [:new, :index, :update, :create, :destroy]
+    resources :comments, only: [:update, :create]
+    resources :categorys, only: [:index, :create, :edit, :update]
+    resources :follows, only: [:create, :index, :update]
+  end
+
+devise_for :admin, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
+
+  namespace :admin do
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :users, only: [:index, :show, :edit, :update]
+    resources :reviews, only: [:new, :index, :update, :create, :destroy]
+    resources :commens, only: [:show, :update]
+    resources :categorys, only: [:index, :create, :edit, :update]
+ end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
